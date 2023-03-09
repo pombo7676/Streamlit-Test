40ba9faa3971ecdab2de7e4f8e234954a8b56362
Iñigo Larrea Pombo <114177420+pombo7676@users.noreply.github.com>
2023-03-09 16:03:10 +0100

    Add files via upload

---
+++ Streamlit/streamlit_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+# Findmore emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+
+# -------- HEADER SECTION ---------
+
+st.subheader("Hi, I am Iñigo :wave:")
+st.title("A Data Analyst from Spain")
+st.write("I am passionate")
+st.write("[Learn more>](https://python)")
