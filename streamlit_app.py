a0a9a81691cc98bc711b362f95cf1154e9e0b02d
Iñigo Larrea Pombo <114177420+pombo7676@users.noreply.github.com>
2023-03-10 10:57:33 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 
 # Findmore emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
@@ -7,7 +7,19 @@ st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
 # -------- HEADER SECTION ---------
 
-st.subheader("Hi, I am Iñigo :wave:")
-st.title("A Data Analyst from Spain")
-st.write("I am passionate")
-st.write("[Learn more>](https://www.tinsa.es/)")
+
+st.image("https://www.tinsa.es/wp-content/uploads/2016/03/logo-tinsa-facebook.jpg")
+st.title("TINSA HR HEADCOUNT")
+st.subheader("App to update registrations and cancellations")
+
+st.write("[Official Website](https://www.tinsa.es/)")
+
+reg_can = st.selectbox(
+    'Type of change?',
+    ('Registration', 'Voluntary Resignation', 'Dismisall'))
+
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
