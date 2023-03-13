b720dc559d05d15468924f161a7773ea4c0475d1
Iñigo Larrea Pombo <114177420+pombo7676@users.noreply.github.com>
2023-03-13 12:53:50 +0100

    Add files via upload

--- HR_Telework.py
+++ HR_Telework.py
@@ -21,15 +21,15 @@ st.title("TINSA TELEWORK WEBSITE")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.image("https://ncrefuge.org/wp-content/uploads/2019/11/new-icon.png")
+    #st.image("https://ncrefuge.org/wp-content/uploads/2019/11/new-icon.png")
     op1 = st.button('Nueva Petición')  
 
 with col2:
-    st.image("https://static9.depositphotos.com/1003938/1123/v/950/depositphotos_11233144-stock-illustration-funny-cartoon-manager.jpg")
+    #st.image("https://static9.depositphotos.com/1003938/1123/v/950/depositphotos_11233144-stock-illustration-funny-cartoon-manager.jpg")
     op2 = st.button('Managers')
 
 with col3:
-    st.image("https://play-lh.googleusercontent.com/WL9oSrJxfO6XDrSnuERVcjFXN--XztDibPGtAxIJsJBfm2ZAv4WvkR5yFuOcFKKR0_A=w240-h480-rw")
+    #st.image("https://play-lh.googleusercontent.com/WL9oSrJxfO6XDrSnuERVcjFXN--XztDibPGtAxIJsJBfm2ZAv4WvkR5yFuOcFKKR0_A=w240-h480-rw")
     op3 = st.button('Estado Petición')
     
     
