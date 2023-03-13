26ab3a3ea8bab040220bdbc651fa10fa708c75dd
Iñigo Larrea Pombo <114177420+pombo7676@users.noreply.github.com>
2023-03-13 14:52:42 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,36 +90,3 @@ if agree == 1:
             data=csv,
             file_name='Test_'+date+'.csv',
             mime='text/csv')
-
-from email.message import EmailMessage
-import ssl
-import smtplib
-
-
-email_emisor = 'inigo.larrea@tinsa.com'
-email_contrasena = 'ndlcrxjymcgfiojr'
-
-email_receptor = 'pombo7676@gmail.com'
-
-asunto = 'Test python'
-cuerpo = """
-Vamos a ver sí funciona este programa de python
-
-
-
-
-"""
-
-
-em = EmailMessage()
-
-em['From'] = email_emisor
-em['To'] = email_receptor
-em['Subject'] = asunto
-em.set_content(cuerpo)
-
-contexto = ssl.create_default_context()
-
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=contexto) as smtp:
-    smtp.login(email_emisor, email_contrasena)
-    smtp.sendmail(email_emisor, email_receptor, em.as_string())
